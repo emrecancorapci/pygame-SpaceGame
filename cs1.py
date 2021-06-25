81228c16de27d377389ff6dea1ed957ca8717e11
emrecancorapci <emrecancorapci@gmail.com>
2021-06-26 01:00:07 +0300

    # Update 0.001 # 26.06.2021
    - Images transferred to newly created "images" folder.
    - New entity called "Player" created.
    - Game clock added but it's not necessary.
    - Score added to top-left corner.
    - When you fail there shall be seen "Game Over!"
    - Game breaking bugs fixed.
    - Codes little bit improved.
    
    Known bugs :
    - Right know there can be only one bullet at screen.
      I have to learn how to create multiple bullet immediately.
    - When mouse moving game is slowing. Kinda annoying.
    - There are no sounds but I promise there will be.

--- cs1.py
+++ cs1.py
@@ -1,4 +1,4 @@
-import pygame as pg
+import pygame as py
 import random
 
 
@@ -38,7 +38,7 @@ class Entity:
 
     def img_load(self, center=False, scale=False):
         # Loads image
-        self.img = pg.image.load(self.path)
+        self.img = py.image.load(self.path)
         if center or scale:
             self.img_transform(center=center, scale=scale)
 
@@ -50,18 +50,31 @@ class Entity:
         # Scales to given values or original values
         if scale:
             if w == 0 or h == 0:
-                self.load = pg.transform.scale(self.load, (self.w, self.h))
+                self.img = py.transform.scale(self.img, (self.w, self.h))
             else:
-                self.load = pg.transform.scale(self.load, (w, h))
+                self.img = py.transform.scale(self.img, (w, h))
 
+class Player(Entity):
+    def __init__(self, name, image, x, y, w, h, speed):
+        super().__init__(name, image, x, y, w, h, speed)
+        self.bullet = []
 
 class Bullet(Entity):
     def __init__(self, name, image, x, y, w, h, speed):
         super().__init__(name, image, x, y, w, h, speed)
-        self.ready = 0
-        self.fire_rate = 0
+        # When ready equals zero ship will fire.
+        self.ready = 0.0
+        # Fire rate of ship
+        self.fire_rate = 0.004
         self.is_exist = False
 
+class Enemy(Entity):
+    def __init__(self, name, image, x, y, w, h, speed):
+        super().__init__(name, image, x, y, w, h, speed)
+        self.spawn = 0
+        self.generate_rate = 0.001
+        self.rate_increase = 0.0001
+        self.number = 0
 
 def collusion(a, b):
     posAx, posAy = a.pos()
@@ -74,103 +87,135 @@ def collusion(a, b):
 
 
 game = {"name": "Space Invaders",
-        "icon": "icon.png",
-        "bg": "bg.jpg",
+        "icon": "images/icon.png",
+        "bg": "images/bg.jpg",
         "w": 1024,
         "h": 768,
         "score": 0,
         "bgc": (255, 255, 255)}
 
 # Initialize the pygame
-pg.init()
+py.init()
 
 # Title and Icon
-pg.display.set_caption(game["name"])
-pg.display.set_icon(pg.image.load(game["icon"]))
+py.display.set_caption(game["name"])
+py.display.set_icon(py.image.load(game["icon"]))
 
 # Creates screen
-screen = pg.display.set_mode((game["w"], game["h"]))
-background = pg.image.load(game["bg"])
+screen = py.display.set_mode((game["w"], game["h"]))
+background = py.image.load(game["bg"])
+
+# Clock
+clock = py.time.Clock()
+
+# Font
+font = []
+for size in range(1, 256):
+    font.append(py.font.Font("freesansbold.ttf", size))
 
 # Background
-bgImg = pg.image.load(game["bg"])
-bgImg = pg.transform.scale(bgImg, (game["w"], game["h"]))
+bgImg = py.image.load(game["bg"])
+bgImg = py.transform.scale(bgImg, (game["w"], game["h"]))
 
-player = Entity("Player", "icon.png", 512, 684, 64, 64, 0.6)
-enemy = Entity("Enemy", "character.png", random.randint(64, 980), random.randint(-100, -64), 64, 64, 0.4)
-fire = Bullet("Shot", "fire.png", player.x, player.y, 8, 8, 1)
+player = Player("Player", "images/icon.png", 512, 684, 64, 64, 0.6)
+enemy = Enemy("Enemy", "images/character.png", random.randint(64, 980), random.randint(-100, -64), 64, 64, 0.4)
+fire = Bullet("Shot", "images/fire.png", player.x, player.y, 8, 8, 1)
 
 # Player
-# *Load
+# *Load and transform
 player.img_load(center=True, scale=True)
 
 # Enemy
-# *Scale
+# *Load and transform
 enemy.img_load(center=True, scale=True)
 
 # Fire
 # *Scale
 fire.img_load(scale=True)
-fire_ready = 0.0
 
 # Game Loop
 running = True
 while running:
-    for event in pg.event.get():
+    clock.tick(1000)
+    for event in py.event.get():
         # Quit from cross
-        if event.type == pg.QUIT:
+        if event.type == py.QUIT:
             running = False
 
     # screen.fill((0, 0, 0)) BG with color
     screen.blit(bgImg, (0, 0))
+
+    """enemy.spawn += enemy.generate_rate
+    enemy.generate_rate += enemy.rate_increase
+    print(enemy.spawn)
+    if enemy.spawn <= 1:
+        enemy[enemy.number] = Enemy("Enemy", "images/character.png", player.x, player.y, 8, 8, 1)
+        screen.blit(enemy[enemy.number].img, (enemy.x, enemy.y))
+        enemy.number += 1
+        enemy.spawn = 0"""
+
     # Player controls
     # *Movement
-    if pg.key.get_pressed()[pg.K_RIGHT] and player.x < game["w"] - player.w:
+    keys = py.key.get_pressed()
+    if keys[py.K_RIGHT] and player.x < game["w"] - player.w:
         player.x += player.v
-    if pg.key.get_pressed()[pg.K_LEFT] and 0 < player.x:
+    if keys[py.K_LEFT] and 0 < player.x:
         player.x -= player.v
-    if pg.key.get_pressed()[pg.K_DOWN] and player.y < game["h"] - player.h:
+    if keys[py.K_DOWN] and player.y < game["h"] - player.h:
         player.y += player.v
-    if pg.key.get_pressed()[pg.K_UP] and 0 < player.y:
+    if keys[py.K_UP] and 0 < player.y:
         player.y -= player.v
     # *Fire
-    if pg.key.get_pressed()[pg.K_SPACE] and player.is_exist and fire_ready >= 100:
+    if keys[py.K_SPACE] and player.is_exist and fire.ready >= 1:
         fire.x = player.x + player.w / 2
         fire.img_transform(center=True)
         fire.y = player.y
         fire.is_exist = True
         fire.ready = 0
-    if fire.ready < 100:
+    if fire.ready < 1:
         fire.ready += fire.fire_rate
 
     # Collisions
     # *Enemy and fire
     if collusion(enemy, fire):
+        # They will never know where it will come from.
         enemy.x, enemy.y = random.randint(64, 960), random.randint(-100, -64)
-        screen.blit(enemy.load, (enemy.x, enemy.y))
+        screen.blit(enemy.img, (enemy.x, enemy.y))
         fire.is_exist = False
         game["score"] += 1
         print("Score :", game["score"])
 
-    # *Player and enemy
-    if collusion(enemy, player):
-        player.x, player.y = 0, 0
-        player.is_exist = False
-        print("Game over!")
-        print("Score :", game["score"])
-
     # Entity behaviours
     if enemy.is_exist:
+        screen.blit(enemy.img, (enemy.x, enemy.y))
+        # Enemies goes down brrrrr
         enemy.y += enemy.v
-        screen.blit(enemy.load, (enemy.x, enemy.y))
+        # If enemies goes down too much, they're gonna get destroyed.
+        if game["h"] < enemy.y < 0 - enemy.h:
+            enemy.is_exist = False
 
     if player.is_exist:
-        screen.blit(player.load, (player.x, player.y))
+        screen.blit(player.img, (player.x, player.y))
+        if collusion(enemy, player):
+            player.is_exist = False
+            print("Game over!")
+            print("Score :", game["score"])
 
     if fire.is_exist:
-        screen.blit(fire.load, (fire.x, fire.y))
+        screen.blit(fire.img, (fire.x, fire.y))
+        # Fires goes up brrrrr
         fire.y -= fire.v
-        if fire.x < 0 - fire.w:
+        # If fires goes up too much, they're gonna get destroyed.
+        if fire.y < 0 - fire.h:
             fire.is_exist = False
 
-    pg.display.update()
+    # Score show
+    score_text = font[32].render("Score : " + str(game["score"]), True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
+    # Game over
+    if not player.is_exist:
+        game_over = font[64].render("Game Over!", True, (255, 255, 255))
+        screen.blit(game_over, (325, 350))
+
+    py.display.update()
